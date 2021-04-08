e60c2f046f73c56cd88eb796087f5e16a813040b
Vadavur <vadavur@gmail.com>
2021-04-08 06:29:07 +0300

    init: solve task-6

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,15 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const mac = n.split('-');
+
+  if (mac.length !== 6) return false;
+  for (let i = 0; i < mac.length; i++) {
+    const regexp = /^[0-9a-f][0-9a-f]$/i;
+    if (!regexp.test(mac[i])) return false;
+  }
+  return true;
 }
 
 module.exports = isMAC48Address;
