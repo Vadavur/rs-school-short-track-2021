3ae9a75e1144e594355303aaddb6df4e96105c14
Vadavur <vadavur@gmail.com>
2021-04-09 00:38:51 +0300

    init: complete task-11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,19 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const letters = str.split('');
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] === letters[i + 1]) {
+      if (typeof letters[i - 1] !== 'number') {
+        letters[i] = 2;
+      } else {
+        letters[i] = letters[i - 1] + 1;
+        letters[i - 1] = '';
+      }
+    }
+  }
+  return letters.join('');
 }
 
 module.exports = encodeLine;
